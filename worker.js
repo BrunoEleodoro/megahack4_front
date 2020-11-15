671fe81c1f9a0507156f9a54465ab9880663704f
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-11-15 20:18:25 -0300

    Updates

--- worker.js
+++ worker.js
@@ -1,48 +1,36 @@
-var CACHE_NAME = 'pwa-task-manager';
+var CACHE_NAME = 'otimigas-app';
+
 var urlsToCache = [
-  '/',
-  '/completed'
+    '/',
+    '/static/js/bundle.js',
+    '/static/js/main.chunk.js',
+    '/static/js/1.chunk.js',
+    '/static/js/0.chunk.js',
+    '/favicon.ico',
+    '/css?family=Open+Sans',
+    '/icon?family=Material+Icons'
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});  
