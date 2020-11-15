8de691ff539c11f7737bb6193be62d84c57e3961
Bruno Eleodoro Roza <Bruno.Eleodoro.Roza@ibm.com>
2020-11-15 20:31:02 -0300

    lets go

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { registerServiceWorker } from './serviceWorker'
 
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-registerServiceWorker();
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
